3810e271cc3dfecee28f4d2eec7c7d98026dcf05
Bill Venables <Bill.Venables@gmail.com>
2008-05-12 00:00:00 +0000

    version 0.93

--- src/poly_mult.c
+++ src/poly_mult.c
@@ -1,4 +1,4 @@
-void poly_mult(double *a, long *na, double *b, long *nb, double *ab) {
+void poly_mult(double *a, int *na, double *b, int *nb, double *ab) {
   int i, j, nab = *na + *nb - 1;
 
   for(i = 0; i < nab; i++) ab[i] = 0.0;
